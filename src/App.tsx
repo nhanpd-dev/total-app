bf04f35409fe94b175ae9502c9fdbcb457979e9a
Phan Dai Nhan <nhanpd@bap.jp>
2024-06-27 17:44:02 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,18 +26,6 @@ const layoutStyle = {
   width: "100%",
   display: "flex",
 };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBkEVokPydqVt1BhWdySkIAoQ4u6X6m140",
-  authDomain: "chat-app-a0220.firebaseapp.com",
-  databaseURL: "https://chat-app-a0220-default-rtdb.firebaseio.com",
-  projectId: "chat-app-a0220",
-  storageBucket: "chat-app-a0220.appspot.com",
-  messagingSenderId: "1096381292862",
-  appId: "1:1096381292862:web:022ba878f061d05a208c6f",
-  measurementId: "G-2H7MM5XGV0",
-};
-
 interface DataType {
   key: string;
   name: string;
